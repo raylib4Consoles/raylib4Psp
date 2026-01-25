ba5aab79b9014cb3f38ac9b06fe01650991d7304
m-c/d <534348+mcidclan@users.noreply.github.com>
2026-01-25 13:35:36 +0100

    add bunnymark sample for PSP using  SDL2 backend

---
+++ pspbuild_sdl2.sh
@@ -0,0 +1,11 @@
+
+## Check if $PSPDEV is set.
+if test ! $PSPDEV; then { echo "ERROR: Set \$PSPDEV installing pspdev toolchain and pspsdk before continuing."; exit 1; } fi
+
+# git clone https://github.com/raylib4Consoles/raylib -b raylib4Consoles
+git clone https://github.com/mcidclan/raylib4Consoles -b raylib4Consoles # tmp
+cd raylib/src
+make PLATFORM=PLATFORM_PSP_SDL clean
+make PLATFORM=PLATFORM_PSP_SDL
+make PLATFORM=PLATFORM_PSP_SDL install
+
---
+++ samples/textures/bunnymark/README.md
@@ -0,0 +1,9 @@
+## PSP:
+```bash
+make clean; make;
+```
+
+## PSP SDL:
+```bash
+make -f Makefile_SDL2 clean; make -f Makefile_SDL2;
+```
