65cfd84a8804d6b2f9ba69da77b1e7bc71d7d533
m-c/d <534348+mcidclan@users.noreply.github.com>
2026-01-25 13:49:17 +0100

    fix typo in sdl2 build.sh

--- pspbuild_sdl2.sh
+++ pspbuild_sdl2.sh
@@ -3,7 +3,7 @@
 if test ! $PSPDEV; then { echo "ERROR: Set \$PSPDEV installing pspdev toolchain and pspsdk before continuing."; exit 1; } fi
 
 # git clone https://github.com/raylib4Consoles/raylib -b raylib4Consoles
-git clone https://github.com/mcidclan/raylib4Consoles -b raylib4Consoles # tmp
+git clone https://github.com/mcidclan/raylib4Consoles -b raylib4Consoles raylib # tmp
 cd raylib/src
 make PLATFORM=PLATFORM_PSP_SDL clean
 make PLATFORM=PLATFORM_PSP_SDL
